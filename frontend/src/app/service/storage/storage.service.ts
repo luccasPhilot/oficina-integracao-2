0f4030c7c07d843cb9ccf65a9ce61ba8794a976c
luccasPhilot <luccasgoncalvrs@gmail.com>
2025-10-18 15:17:25 -0300

    feat: service genérica para usar o storage.

---
+++ frontend/src/app/service/storage/storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+
+import { SessionData } from '../../shared/interfaces/common.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  // Session data variables (names)
+  private readonly sessionData: SessionData = {
+    token: 'access_token',
+  }
+
+  constructor() { }
+
+  // Get Access Token
+  public getToken(): string | null {
+    return sessionStorage.getItem(this.sessionData.token);
+  }
+
+}
---
+++ frontend/src/app/shared/interfaces/common.interface.ts
@@ -0,0 +1,4 @@
+export interface SessionData {
+    token: string;
+    // Add other session data variables as needed
+}
